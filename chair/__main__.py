413225072410e108361ec23d42e016aa3f828239
Timothy du Heaume <timothy.duheaume@gmail.com>
2019-09-17 23:06:23 +0900

    add our intrepid adventurer

--- chair/__main__.py
+++ chair/__main__.py
@@ -1,5 +1,6 @@
 """Core game loop and logic"""
 import pygame
+import entities
 
 
 def main():
@@ -7,7 +8,14 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((640, 480))
 
+    hero_sprite = pygame.image.load("images/hero.png")
+    hero = entities.Hero(100, 200, hero_sprite)
+    actors = pygame.sprite.Group()
+    actors.add(hero)
+
     while True:
+        actors.draw(screen)
+        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
---
+++ chair/entities.py
@@ -0,0 +1,13 @@
+"""Game entities."""
+import pygame
+from pygame import Rect
+
+TILE_SIZE = 32
+
+
+class Hero(pygame.sprite.Sprite):
+    """The player-controlled actor."""
+    def __init__(self, x, y, image):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = image
+        self.rect = Rect(x, y, TILE_SIZE, TILE_SIZE)
