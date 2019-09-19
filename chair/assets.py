61c22bf8a23108cfe04baed3f9333381eff2d6b6
Timothy du Heaume <timothy.duheaume@gmail.com>
2019-09-19 23:55:56 +0900

    refactor the AssetManager a little
    I don't want to have to remember to call .convert_alpha() every time an
    image needs loading.

--- chair/assets.py
+++ chair/assets.py
@@ -7,8 +7,8 @@ from entities import TILE_SIZE
 class AssetManager:
     """A container for managing the loading of assets."""
     def __init__(self):
-        self.hero_sprite = pygame.image.load("images/hero.png").convert_alpha()
-        self._terrain_sprite = pygame.image.load("images/desert.png").convert_alpha()
+        self.hero_sprite = load_image("images/hero.png")
+        self._terrain_sprite = load_image("images/desert.png")
         self.terrain_sprites = [
             self._terrain_sprite.subsurface(pygame.Rect(
                 x * TILE_SIZE,
@@ -18,3 +18,8 @@ class AssetManager:
             for x in range(4)
             for y in range(4)
         ]
+
+
+def load_image(path):
+    """Load and convert an image from the given path."""
+    return pygame.image.load(path).convert_alpha()
