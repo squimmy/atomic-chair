7be2accf76f971aee6ec7ae4791c88648b98fad6
Timothy du Heaume <timothy.duheaume@gmail.com>
2019-09-22 09:34:16 +0900

    moving sprites around was being done all wrong!
    Using the sprite rect to track the position of entities meant that
    sub-pixel positioning was being lost every frame. This meant projectiles
    would gradually go off-course and player motion could be jittery. The
    problem is easily fixed by storing the ideal position of the entity in a
    separate vector and moving the image to align with that vector's
    position each fram. This whole mess is an example of why it's a good
    idea to keep game logic and presentation logic separate.

--- chair/entities.py
+++ chair/entities.py
@@ -16,8 +16,8 @@ class Hero(pygame.sprite.Sprite):
         self.rect = Rect(x, y, TILE_SIZE, TILE_SIZE)
         self.input_manager = input_manager
         self.clock = clock
-        self.inertia = pygame.math.Vector2()
-        self.speed = 0.17
+        self.pos = Vector2(x, y)
+        self.speed = 0.25
         self.projectile_manager = projectile_manager
 
     def update(self):
@@ -26,23 +26,21 @@ class Hero(pygame.sprite.Sprite):
         so that fractional input can be aggregated over multiple frames."""
         input_vector = self.input_manager.get_direction()
         input_vector *= self.clock.get_time() * self.speed
-        self.inertia += input_vector
-        while math.fabs(self.inertia.x) >= 1:
-            self.rect.x += self.inertia.x
-            self.inertia.x -= math.copysign(1, self.inertia.x)
-        while math.fabs(self.inertia.y) >= 1:
-            self.rect.y += self.inertia.y
-            self.inertia.y -= math.copysign(1, self.inertia.y)
+        self.pos += input_vector
+        self.rect.x = self.pos.x
+        self.rect.y = self.pos.y
         if self.input_manager.clicked_buttons[1]:
+            center_x, center_y = self._get_center()
             target_x, target_y = self.input_manager.mouse_pos
-            rel_x = target_x - self.rect.x
-            rel_y = target_y - self.rect.y
+            rel_x = target_x - center_x
+            rel_y = target_y - center_y
             direction = Vector2(rel_x, rel_y)
-            direction.scale_to_length(0.25)
-            self.projectile_manager.add_bullet(
-                self.rect.x,
-                self.rect.y,
-                direction)
+            direction.scale_to_length(0.5)
+            self.projectile_manager.add_bullet(center_x, center_y, direction)
+
+    def _get_center(self):
+        return (self.rect.x + TILE_SIZE / 2,
+                self.rect.y + TILE_SIZE / 2)
 
 
 class Baddie(pygame.sprite.Sprite):
@@ -58,11 +56,15 @@ class Projectile(pygame.sprite.Sprite):
     def __init__(self, x, y, image, velocity, lifetime, clock):
         pygame.sprite.Sprite.__init__(self)
         self.image = image
-        self.rect = Rect(x, y, TILE_SIZE, TILE_SIZE)
+        self.rect = Rect(
+            x - image.get_width() / 2,
+            y - image.get_height() / 2,
+            TILE_SIZE,
+            TILE_SIZE)
         self.velocity = velocity
         self.lifetime = lifetime
         self.clock = clock
-        self.inertia = pygame.math.Vector2()
+        self.pos = pygame.math.Vector2(x, y)
 
     def update(self):
         """Move the projectile and reduce its lifespan."""
@@ -71,13 +73,9 @@ class Projectile(pygame.sprite.Sprite):
         if self.lifetime <= 0:
             self.kill()
         motion = self.velocity * delta_t
-        self.inertia += motion
-        while math.fabs(self.inertia.x) >= 1:
-            self.rect.x += self.inertia.x
-            self.inertia.x -= math.copysign(1, self.inertia.x)
-        while math.fabs(self.inertia.y) >= 1:
-            self.rect.y += self.inertia.y
-            self.inertia.y -= math.copysign(1, self.inertia.y)
+        self.pos += motion
+        self.rect.x = self.pos.x
+        self.rect.y = self.pos.y
 
 
 class ProjectileManager:
